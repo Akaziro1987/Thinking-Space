b99bd65dba4bd776df86a4a52fc3158628cb0581
lenovo <lenovo@DESKTOP-H11UT5P>
2026-08-06 18:57:35 +0100

    TEST

--- Thinking Space/Thinking Space.cpp
+++ Thinking Space/Thinking Space.cpp
@@ -2,14 +2,57 @@
 using namespace std;
 int main()
 {
-	cout << "hear me nigga you need to remember that" << std::endl;
-	cout << "m1 " << "m2 " << "m3 " << std::endl;
+
+	//THIS IS THE STAGE ONE
+
+   /*and this is a long comment so you will never forget about this*/
+
+
+	cout << "hear me nigga you need to remember that" << endl;
+	cout << "m1 " << "m2 " << "m3 " << endl;
 	cout << "m1 m2 m3" << endl;
 	cout << "the results of " << "673 + 364 = " << 673 + 364 << endl;
+
+
+	cout << "NAME: AKAZIRO NAZARIO\n";
+	cout << "AGE: 19\n";
+	cout << "I CAN DO ANYTHING\n";
+	cout << "so can we end this now\n";
+	cout << endl << endl;
+	cout << "this is so far\n\n";
+
+	cout << "this " << "is " << "one " << "message" << endl << endl;
+
+	cout << "*        *\n";
+	cout << "*        *\n";
+	cout << "*        *\n";
+	cout << "**********"<< endl;
+	cout << "*        *\n";
+	cout << "*        *\n";
+	cout << "*        *\n\n\n";
+
+
+
+	/*Stage 2*/
+
+	cout << "this is how you make a double \\ double qoutaion" << endl;
+	cout << " and this is TAB \t qoutation for 4 spaces" << endl;
+
+	cout << "and my name is \"AKAZIRO\" " << endl;
+	cout << " you sure you remember \'THIS\' " << endl;
+	cout << "and this is SOUND \a" << endl << endl;
+
+	cout << "Dear oni \\ HOW ARE YOU?" << endl;
+	cout << "love what you do with \"THE PLACE\" " << endl;
+
+
+
 	return 0;
 }
 
-//THIS WAS THE STAGE ONE
+
+
+
 
 
 
